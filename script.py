4e0dc4a4768e0b00df60fa2a224209601a0eb42f
gangstar1010 <88017311+gangstar1010@users.noreply.github.com>
2021-10-02 16:04:09 +0530

    Update script.py

--- script.py
+++ script.py
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @Mo_Tech_YT</b>
+<b>© @SH_Movies</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : MT Unlimited Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : SH Unlimited Filter Bot</b>
 
-⭕️<b>Creater :</b> <b>@Mo_Tech_YT</b>    
+⭕️<b>Creater :</b> <b>@Shprosearchbot</b>    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
